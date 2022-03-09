958fcec94002ad2b85837a17d9866c5bc6afc299
jwcbmat <johnathan.wcb@gmail.com>
2022-03-08 22:00:46 -0300

    once and randomUUID

--- src/server.js
+++ src/server.js
@@ -1,7 +1,31 @@
 import { createServer } from 'http';
+import { once } from 'events';
+import { randomUUID } from 'crypto';
+const Database = new Map();
+
+function respondJSON(data, response) {
+    return response.end(JSON.stringify(data));
+}
 
 async function handler(request, response) {
-    response.end('hello world');
+    const { method } = request;
+
+    if (method === 'GET') {
+
+        return respondJSON([...Database.values()], response)
+    };
+
+    if (method === 'POST') {
+        const body = JSON.parse(await once(request, 'data'));
+        const id = randomUUID();
+        console.log('recebido', body);
+        return respondJSON({ ok: 1 }, response)
+    };
+
+    if (method === 'DELETE') {
+
+        return respondJSON({ ok: 1 }, response)
+    }
 }
 
 export default createServer(handler);
