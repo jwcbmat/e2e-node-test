dea15462a7d3c861aeeb2d7585a00f80c9a93185
jwcbmat <johnathan.wcb@gmail.com>
2022-03-09 12:15:16 -0300

    initial test and node_options in jest

---
+++ teste/e2e/api.test.js
@@ -0,0 +1,9 @@
+import {
+    jest, expect, test, describe
+} from '@jest/globals';
+
+describe('API E2E Test Suite', () => {
+    test.todo('GET / - Should return an array');
+    test.todo('POST / - Should save an item and return ok');
+    test.todo('DELETE / - Should return an array');
+}) 
